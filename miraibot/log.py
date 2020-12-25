4143aeb4a4548390b931be6d34bd05ba3ef0187b
ieew <44091335+ieew@users.noreply.github.com>
2020-12-25 17:25:13 +0800

    记录部分等级的日志到文件
    将 error 和以上的日志记录到文件的同时也在控制台显示这些日志

--- miraibot/log.py
+++ miraibot/log.py
@@ -1,12 +1,20 @@
 import logging
 import sys
 
-logger = logging.getLogger()
+logger = logging.getLogger("miraibot")
+fms="[%(asctime)s] %(levelname)s : %(message)s"
+datefmt="%Y-%m-%d %H:%M:%S"
+
 default_handler = logging.StreamHandler(sys.stdout)
-default_handler.setFormatter(
-    logging.Formatter('[%(asctime)s] %(levelname)s :  %(message)s', datefmt="%Y-%m-%d %H:%M:%S"))
+default_handler.setLevel(logging.INFO)
+default_handler.setFormatter(logging.Formatter(fms, datefmt=datefmt))
 logger.addHandler(default_handler)
 
+handler = logging.FileHandler("error.log")
+handler.setLevel(logging.ERROR)
+handler.setFormatter(logging.Formatter(fms,datefmt=datefmt))
+logger.addHandler(handler)
+
 __all__ = [
-    'logger',
+    "logger"
 ]
