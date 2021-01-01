c17724ae51813f092dbc0342dca8b6b1ea427b9a
ieew <i@ieew.cc>
2021-01-01 17:04:48 +0800

    修改范例插件

--- plugins/baidu.py
+++ plugins/baidu.py
@@ -1,5 +1,5 @@
 from miraibot import GraiaMiraiApplication, get
-from miraibot.message import Group, MessageChain, Plain
+from miraibot.message import Group, MessageChain, Plain, Quote,Source
 from config import QQ
 
 import re
@@ -11,35 +11,14 @@ bcc = get.bcc()
 @bcc.receiver("GroupMessage")
 async def friend_message_listener(app: GraiaMiraiApplication, group: Group, message: MessageChain):
     m = message.asDisplay()
-    if m.endswith('帮楼上百度'):
-        if message.__root__[1].senderId != QQ:
-            red = re.search(r'^\[\[.+?]]', m)
-            if red is not None:
-                red = re.search(r'(?<=text=).+?(?=]])', red.group())
-                if red is not None:
-                    data = await app.sendGroupMessage(
-                        group,
-                        MessageChain.create(
-                            [
-                                Plain(f'请在这里找答案哦:\nhttps://www.baidu.com/s?wd={parse.quote(f"{red.group()}")}')
-                            ]),
-                        quote=message.__root__[1].origin.__root__[0]
-                    )
-                    if data['code'] != 0:
-                        await app.sendGroupMessage(
-                            group,
-                            MessageChain.create(
-                                [
-                                    Plain('竹竹没能在手机上找到这条消息呢~')
-                                ]
-                            )
-                        )
-        else:
-            await app.sendGroupMessage(
-                group,
-                MessageChain.create(
-                    [
-                        Plain('竹竹知道自己说的是什么东西的啦~')
-                    ]
-                )
-            )
+    if m.endswith("帮楼上百度"):
+        if message.has(Quote):
+            quote: Quote = message.get(Quote).pop(0)
+            source = message.get(Source).pop(0)
+            if quote.senderId != QQ:
+                ctx = MessageChain.create(quote.origin.get(Plain)).asDisplay()
+                while ctx.startswith(" "):
+                    ctx = ctx[1:]
+                await app.sendGroupMessage(group, MessageChain.create([Plain("请在这里找答案哦:\n"), Plain("https://www.baidu.com/s?wd="), Plain(parse.quote(ctx))]), quote=source)
+            else:
+                await app.sendGroupMessage(group, MessageChain.create([Plain("竹竹知道自己说的是什么东西啦~")]))
