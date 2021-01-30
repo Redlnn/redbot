6c44f3c3b2b2d7c283dd9a79ba237976087abaaf
root <root@ubuntu20.04>
2021-01-30 20:07:34 +1100

    _

--- miraibot/log.py
+++ miraibot/log.py
@@ -2,8 +2,8 @@ import logging
 import sys
 
 logger = logging.getLogger("miraibot")
-fms="[%(asctime)s] %(levelname)s : %(message)s"
-datefmt="%Y-%m-%d %H:%M:%S"
+fms = "[%(asctime)s] %(levelname)s : %(message)s"
+datefmt = "%Y-%m-%d %H:%M:%S"
 
 default_handler = logging.StreamHandler(sys.stdout)
 default_handler.setFormatter(logging.Formatter(fms, datefmt=datefmt))
@@ -11,7 +11,7 @@ logger.addHandler(default_handler)
 
 handler = logging.FileHandler("error.log")
 handler.setLevel(logging.ERROR)
-handler.setFormatter(logging.Formatter(fms,datefmt=datefmt))
+handler.setFormatter(logging.Formatter(fms, datefmt=datefmt))
 logger.addHandler(handler)
 
 __all__ = [
