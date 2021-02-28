2339c9c6b1011a23cfece4832d05aaf7733d6ff9
ieew <i@ieew.cc>
2021-02-28 21:50:30 +0800

    无关紧要的微调

--- plugins/baidu.py
+++ plugins/baidu.py
@@ -1,15 +1,19 @@
 from miraibot import GraiaMiraiApplication, get
-from miraibot.message import Group, MessageChain, Plain, Quote,Source
+from miraibot.message import Group, MessageChain, Plain, Quote, Source
 from config import QQ
 
-import re
 from urllib import parse
 
 bcc = get.bcc()
+__plugin_name__ = "帮楼上百度"
+__name__ = "帮楼上百度"
 
 
 @bcc.receiver("GroupMessage")
-async def friend_message_listener(app: GraiaMiraiApplication, group: Group, message: MessageChain):
+async def friend_message_listener(
+    app: GraiaMiraiApplication,
+    group: Group, message: MessageChain
+):
     m = message.asDisplay()
     if m.endswith("帮楼上百度"):
         if message.has(Quote):
@@ -19,6 +23,16 @@ async def friend_message_listener(app: GraiaMiraiApplication, group: Group, mess
                 ctx = MessageChain.create(quote.origin.get(Plain)).asDisplay()
                 while ctx.startswith(" "):
                     ctx = ctx[1:]
-                await app.sendGroupMessage(group, MessageChain.create([Plain("请在这里找答案哦:\n"), Plain("https://www.baidu.com/s?wd="), Plain(parse.quote(ctx))]), quote=source)
+                await app.sendGroupMessage(
+                    group,
+                    MessageChain.create([
+                        Plain("请在这里找答案哦:\n"),
+                        Plain("https://www.baidu.com/s?wd="),
+                        Plain(parse.quote(ctx))
+                    ]),
+                    quote=source
+                )
             else:
-                await app.sendGroupMessage(group, MessageChain.create([Plain("竹竹知道自己说的是什么东西啦~")]))
+                await app.sendGroupMessage(
+                    group, MessageChain.create([Plain("竹竹知道自己说的是什么东西啦~")])
+                )
