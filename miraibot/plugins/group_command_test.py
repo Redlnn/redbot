44176f56e619d9f7a18cc4e2308501088d516a1c
Redlnn <41742767+Redlnn@users.noreply.github.com>
2021-08-26 16:04:36 +0800

    指令系统 - 增加一个内置的示例插件

---
+++ miraibot/plugins/group_command_test.py
@@ -0,0 +1,32 @@
+"""
+群指令系统示例插件
+示例用法:
+@bot test
+@bot t
+@bot tt
+"""
+from miraibot import get, GraiaMiraiApplication
+from miraibot.event import Member, MemberPerm, Group, MessageChain, GroupMessage
+from miraibot.command import group_command
+
+
+bcc = get.bcc()
+__plugin_name__ = "群指令系统示例插件"
+__plugin_usage__ = "群指令系统测试，可用命令：test、t、tt，需要的权限为管理员或群组，需要机器人被At"
+
+
+@group_command(
+    command='test',
+    aliases=['t', 'tt'],
+    group=[123456789],
+    permission=[MemberPerm.Administrator, MemberPerm.Owner],
+    at=True
+)
+async def group_command_listener(
+    app: GraiaMiraiApplication,
+    group: Group,
+    message: MessageChain,
+    member: Member,
+    event: GroupMessage
+):
+    print(app, group, message, member, event, sep='\n')
