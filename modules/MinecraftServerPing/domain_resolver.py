fd4969d4e168bd93acfceb638194f9aed76b7a6f
Redlnn <w731347477@gmail.com>
2021-12-06 21:03:44 +0800

    fix: dns解析CNAME记录出错

--- modules/MinecraftServerPing/domain_resolver.py
+++ modules/MinecraftServerPing/domain_resolver.py
@@ -10,6 +10,7 @@ from dns.resolver import NXDOMAIN, NoAnswer
 __all__ = ["domain_resolver", "domain_resolver_srv"]
 
 
+# A 记录或 CNAME 记录
 def domain_resolver(domain: str) -> bool | str:
     dns_resolver = dns.resolver.Resolver()
     dns_resolver.nameservers = ['119.29.29.29']
@@ -17,12 +18,10 @@ def domain_resolver(domain: str) -> bool | str:
         resolve_result = dns_resolver.resolve(domain, 'A', tcp=True, lifetime=5)
     except (NoAnswer, NXDOMAIN):
         return False
-    nameserver_answer = resolve_result.response.answer
-    target_ip = nameserver_answer[0][0].address
-    return target_ip
+    return resolve_result[0].to_text()
 
 
-def domain_resolver_srv(domain: str) -> Tuple[bool | str, bool | str]:
+def domain_resolver_srv(domain: str) -> Tuple[bool | str, bool | int]:
     srv_domain = f'_minecraft._tcp.{domain}'
     dns_resolver = dns.resolver.Resolver()
     dns_resolver.nameservers = ['119.29.29.29']
@@ -30,9 +29,8 @@ def domain_resolver_srv(domain: str) -> Tuple[bool | str, bool | str]:
         resolve_result = dns_resolver.resolve(srv_domain, 'SRV', tcp=True, lifetime=5)
     except (NoAnswer, NXDOMAIN):
         return False, False
-    nameserver_answer = resolve_result.response.answer
-    if nameserver_answer[0][0].rdtype == RdataType.SRV:
-        host = str(nameserver_answer[0][0].target).rstrip('.')
-        port = nameserver_answer[0][0].port
+    if resolve_result.rdtype == RdataType.SRV:
+        host: str = resolve_result[0].target.to_text().strip('.')
+        port: int = resolve_result[0].port
         return host, port
     return False, False
