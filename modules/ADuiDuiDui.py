12d2d5f31bc15778de0d69961d644a50fd4af1aa
Redlnn <w731347477@gmail.com>
2022-01-31 22:10:30 +0800

    feat: 啊对对对

---
+++ modules/ADuiDuiDui.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os.path import basename
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Plain
+from graia.ariadne.message.parser.twilight import RegexMatch, Sparkle, Twilight
+from graia.ariadne.model import Group
+from graia.broadcast.interrupt import InterruptControl
+from graia.saya import Channel, Saya
+from graia.saya.builtins.broadcast import ListenerSchema
+
+from utils.config import get_modules_config
+from utils.control.permission import GroupPermission
+from utils.module_register import Module
+
+saya = Saya.current()
+channel = Channel.current()
+inc = InterruptControl(saya.broadcast)
+
+modules_cfg = get_modules_config()
+module_name = basename(__file__)[:-3]
+
+Module(
+    name='啊对对对',
+    file_name=module_name,
+    author=['Red_lnn', 'KuborKelp'],
+    usage='啊对对对',
+).register()
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[Twilight(Sparkle([RegexMatch(r'[啊阿]对+')]))],
+        decorators=[GroupPermission.require()],
+    )
+)
+async def main(app: Ariadne, group: Group, message: MessageChain):
+    if module_name in modules_cfg.disabledGroups:
+        if group.id in modules_cfg.disabledGroups[module_name]:
+            return
+    await app.sendGroupMessage(group, message + MessageChain.create(Plain('对')))
