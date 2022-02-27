e2a945bf294eb0bc7d3cc7465e2e319e32f05fa9
Redlnn <w731347477@gmail.com>
2022-02-27 15:24:01 +0800

    fix error_handler

--- core_modules/error_handelr.py
+++ core_modules/error_handelr.py
@@ -4,6 +4,7 @@
 import traceback
 from io import StringIO
 
+from graia.ariadne import get_running
 from graia.ariadne.app import Ariadne
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import Image, Plain
@@ -12,26 +13,33 @@ from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 
 from util.config import basic_cfg
-from util.text2img import async_generate_img
+from util.text2img import Text2ImgConfig, async_generate_img
 
 channel = Channel.current()
 
 
 @channel.use(ListenerSchema(listening_events=[ExceptionThrowed]))
-async def except_handle(app: Ariadne, event: ExceptionThrowed):
+async def except_handle(event: ExceptionThrowed):
     if isinstance(event.event, ExceptionThrowed):
         return
-    else:
-        with StringIO() as fp:
-            traceback.print_tb(event.exception.__traceback__, file=fp)
-            tb = fp.getvalue()
-        msg = [
-            f'异常事件：\n{str(event.event)}\n'
-            f'异常类型：\n{type(event.exception)}\n'
-            f'异常内容：\n{str(event.exception)}\n'
-            f'异常追踪：\n{tb}'
-        ]
-        img_bytes = await async_generate_img(msg, 100)
-        await app.sendFriendMessage(
-            basic_cfg.admin.masterId, MessageChain.create(Plain('发生异常\n'), Image(data_bytes=img_bytes))
-        )
+    app = get_running(Ariadne)
+    with StringIO() as fp:
+        traceback.print_tb(event.exception.__traceback__, file=fp)
+        tb = fp.getvalue()
+    msg = [
+        f'异常事件：\n{str(event.event)}\n'
+        f'异常类型：\n{type(event.exception)}\n'
+        f'异常内容：\n{str(event.exception)}\n'
+        f'异常追踪：\n{tb}'
+    ]
+    img_bytes = await async_generate_img(msg, Text2ImgConfig(CharsPerLine=80))
+    await app.sendFriendMessage(
+        basic_cfg.admin.masterId, MessageChain.create(Plain('发生异常\n'), Image(data_bytes=img_bytes))
+    )
+
+
+# from graia.ariadne.event.message import GroupMessage
+# @channel.use(ListenerSchema(listening_events=[GroupMessage]))
+# async def test(msg: MessageChain):
+#     if msg.asDisplay() == 'error_handler_test':
+#         raise ValueError('test')
