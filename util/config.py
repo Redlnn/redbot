c3e47ecb0c01a8150dce5f2f92e5fcdea9cdd7f1
Redlnn <w731347477@gmail.com>
2022-02-28 23:33:11 +0800

    Rconig 允许当 Pydantic 的 BaseModel 用

--- util/config.py
+++ util/config.py
@@ -10,14 +10,18 @@ from .path import config_path, data_path
 
 
 class RConfig(BaseModel):
-    __filename__: str  # 无需指定后缀
+    __filename__: str | None = None  # 无需指定后缀，当实例化时不传入该参数则与 BaseModel 无异
     __in_data_folder__: bool = False
 
     def __init__(self, **data) -> None:
-        if self.__in_data_folder__:
+        if self.__filename__ is None:
+            super().__init__(**data)
+            return
+        elif self.__in_data_folder__:
             path = Path(data_path, f'{self.__filename__}.json')
         else:
             path = Path(config_path, f'{self.__filename__}.json')
+
         if not path.exists():
             super().__init__(**data)
             with open(path, 'w') as f:
@@ -31,7 +35,9 @@ class RConfig(BaseModel):
             super().__init__(**file_data)
 
     def save(self) -> None:
-        if self.__in_data_folder__:
+        if self.__filename__ is None:
+            raise ValueError('__filename__ is not defined')
+        elif self.__in_data_folder__:
             path = Path(data_path, f'{self.__filename__}.json')
         else:
             path = Path(config_path, f'{self.__filename__}.json')
@@ -39,7 +45,9 @@ class RConfig(BaseModel):
             f.write(self.json(indent=2, ensure_ascii=False))
 
     def reload(self) -> None:
-        if self.__in_data_folder__:
+        if self.__filename__ is None:
+            raise ValueError('__filename__ is not defined')
+        elif self.__in_data_folder__:
             path = Path(data_path, f'{self.__filename__}.json')
         else:
             path = Path(config_path, f'{self.__filename__}.json')
@@ -48,13 +56,13 @@ class RConfig(BaseModel):
         super().__init__(**data)
 
 
-class MAHConfig(BaseModel):
+class MAHConfig(RConfig):
     account: int
     host: AnyHttpUrl = 'http://localhost:8080'
     verifyKey: str
 
 
-class AdminConfig(BaseModel):
+class AdminConfig(RConfig):
     masterId: int = 731347477  # 机器人主人的QQ号
     masterName: str = 'Red_lnn'
     admins: list[int] = [731347477]
