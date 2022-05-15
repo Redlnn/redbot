a31a26d395f88dc92f73f02f271ce827f62dc177
Redlnn <w731347477@gmail.com>
2022-05-16 03:08:44 +0800

    feat: 尝试使用新的添加 fastapi 路由的方式

---
+++ removed_modules/fastapi/api/group/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from contextvars import ContextVar
+
+from graia.ariadne import get_running
+from graia.ariadne.app import Ariadne
+from graia.ariadne.model import MemberPerm
+
+from util.fastapi_core.response_model import GeneralResponse
+from util.fastapi_core.router import Router
+
+perm_map = {
+    MemberPerm.Member: '群成语',
+    MemberPerm.Administrator: '管理员',
+    MemberPerm.Owner: '群主',
+}
+
+group_list_cache: ContextVar[dict[int, str | int]] = ContextVar('group_list_cache')
+last_get_group_list_time: ContextVar[float] = ContextVar('last_get_group_list_time')
+
+
+@Router.get('/api/get_group_list', response_model=GeneralResponse)
+async def get_group_list():
+    last = last_get_group_list_time.get(None)
+    if last is not None and (time.time() - last) < 120:
+        tmp = group_list_cache.get(None)
+        if tmp is not None:
+            return GeneralResponse(data=tmp)
+
+    last_get_group_list_time.set(time.time())
+
+    app = get_running(Ariadne)
+    group_list = await app.getGroupList()
+    tmp = {}
+    for group in group_list:
+        tmp[group.id] = {
+            'id': group.id,
+            'name': group.name,
+            'accountPerm': perm_map[group.accountPerm],
+        }
+    group_list_cache.set(tmp)
+    return GeneralResponse(data=tmp)
---
+++ util/fastapi_core/router.py
@@ -0,0 +1,33 @@
+from typing import Any, Callable, Coroutine
+
+from pydantic import BaseModel
+
+
+class Route(BaseModel):
+    path: str
+    methods: list[str]
+    endpoint: Callable[..., Coroutine[Any, Any, Any]]
+    response_model: Any
+    kwargs: dict[str, Any]
+
+
+class Router:
+    routes: list[Route] = []
+
+    @classmethod
+    def register(cls, path: str, methods: list[str], response_model, **kwargs):
+        def wrapper(func: Callable[..., Coroutine[Any, Any, Any]]):
+            cls.routes.append(
+                Route(path=path, methods=methods, endpoint=func, response_model=response_model, kwargs=kwargs)
+            )
+            return func
+
+        return wrapper
+
+    @classmethod
+    def get(cls, path: str, response_model, **kwargs):
+        return cls.register(path, ['GET'], response_model=response_model, **kwargs)
+
+    @classmethod
+    def post(cls, path: str, response_model, **kwargs):
+        return cls.register(path, ['POST'], response_model=response_model, **kwargs)
