36ef32d527d0537bff92db4d2d0428e2ccf956b9
Redlnn <w731347477@gmail.com>
2022-12-09 22:38:17 +0800

    全局字体替换 & 修复B站解析白边

--- util/text2img.py
+++ util/text2img.py
@@ -8,6 +8,7 @@ from graiax.text2img.playwright import (
     ScreenshotOption,
     convert_text,
 )
+from graiax.text2img.playwright.renderer import BuiltinCSS
 from jinja2 import Template
 
 from util.fonts_provider import fill_font
@@ -22,6 +23,21 @@ footer = (
 html_render = HTMLRenderer(
     page_option=PageOption(device_scale_factor=1.5),
     screenshot_option=ScreenshotOption(type='jpeg', quality=80, full_page=True, scale='device'),
+    css=(
+        BuiltinCSS.reset,
+        BuiltinCSS.github,
+        BuiltinCSS.one_dark,
+        BuiltinCSS.container,
+        "@font-face{font-family:'harmo';font-weight:300;"
+        "src:url('http://static.graiax/fonts/HarmonyOS_Sans_SC_Light.ttf') format('truetype');}"
+        "@font-face{font-family:'harmo';font-weight:400;"
+        "src:url('http://static.graiax/fonts/HarmonyOS_Sans_SC_Regular.ttf') format('truetype');}"
+        "@font-face{font-family:'harmo';font-weight:500;"
+        "src:url('http://static.graiax/fonts/HarmonyOS_Sans_SC_Medium.ttf') format('truetype');}"
+        "@font-face{font-family:'harmo';font-weight:600;"
+        "src:url('http://static.graiax/fonts/HarmonyOS_Sans_SC_Bold.ttf') format('truetype');}"
+        "*{font-family:'harmo',sans-serif}",
+    ),
     page_modifiers=[
         lambda page: page.route(lambda url: bool(re.match('^http://static.graiax/fonts/(.+)$', url)), fill_font)
     ],
